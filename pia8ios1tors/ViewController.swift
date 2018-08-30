1d0e736ab01a6b5c529415ad38756c39e1b12e36
Majad In <majad@bobbys-macbook-pro.local>
2018-08-30 09:13:38 +0200

    Initial Commit

---
+++ pia8ios1tors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  pia8ios1tors
+//
+//  Created by Majad In on 2018-08-30.
+//  Copyright © 2018 Majad In. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
